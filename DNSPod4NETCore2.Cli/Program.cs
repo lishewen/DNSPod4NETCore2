2847006d8737f130f024b420079106dafacd707d
算神 <lishewen@msn.com>
2018-10-08 08:46:11 +0800

    取消注入模式，推到重写

--- DNSPod4NETCore2.Cli/Program.cs
+++ DNSPod4NETCore2.Cli/Program.cs
@@ -1,41 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace DNSPod4NETCore2.Cli
 {
     class Program
     {
-        static IConfigurationRoot Configuration { get; set; }
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            var builder = new HostBuilder()
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    config.AddEnvironmentVariables();
-
-                    if (args != null)
-                    {
-                        config.AddCommandLine(args);
-                    }
-
-                    Configuration = config.Build();
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddOptions();
-                    DnsPodConfiguration configuration = Configuration.Get<DnsPodConfiguration>();
-                    services.AddSingleton(configuration);
-                    services.AddHttpClient<IDnsPodClient, DnsPodClient>();
-                })
-                .ConfigureLogging(a =>
-                {
-                    a.SetMinimumLevel(LogLevel.None);
-                });
-
-            await builder.RunConsoleAsync();
+            Console.WriteLine("Hello World!");
         }
     }
 }
