541a62b572445765d3d336b1ddc3b773931a8bf5
算神 <lishewen@msn.com>
2018-10-08 09:47:05 +0800

    cli完成

---
+++ DNSPod4NETCore2.Cli/CommandLineArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace DNSPod4NETCore2.Cli
+{
+    public class CommandLineArgs
+    {
+        public string Token { get; set; }
+        public string DomainName { get; set; }
+        public int RecordId { get; set; }
+        public string Value { get; set; }
+        public string SubDomain { get; set; }
+        public string RecordType { get; set; }
+    }
+}
--- DNSPod4NETCore2.Cli/Program.cs
+++ DNSPod4NETCore2.Cli/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace DNSPod4NETCore2.Cli
 {
@@ -6,7 +9,33 @@ namespace DNSPod4NETCore2.Cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("DnsPod Cli Tools v1.0");
+
+            var builder = new ConfigurationBuilder();
+            builder.AddCommandLine(args, new Dictionary<string, string> {
+                {"-t","Token" },
+                {"-d","DomainName" },
+                {"-r","RecordId" },
+                {"-v","Value" },
+                {"-s","SubDomain" },
+                {"--type","RecordType" }
+            });
+            var item = builder.Build().Get<CommandLineArgs>();
+
+            Console.WriteLine("正在修改域名记录:" + item.Token);
+
+            DnsPodConfiguration configuration = new DnsPodConfiguration
+            {
+                Token = item.Token
+            };
+            DnsPodClient client = new DnsPodClient(new HttpClient(), configuration);
+            DnsPodRecord record = new DnsPodRecord(client);
+            if (record.Modify(item.DomainName, item.RecordId, item.Value, item.SubDomain, item.RecordType))
+                Console.WriteLine("修改完成！");
+            else
+                Console.WriteLine("修改失败！");
+
+            //Console.ReadLine();
         }
     }
 }
