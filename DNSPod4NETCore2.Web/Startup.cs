d78e7b2e205a4957ceb4d2d7e4876f22a6d9c527
算神 <lishewen@msn.com>
2019-10-17 14:51:47 +0800

    Web项目改ASP.Net Core 3.0

--- DNSPod4NETCore2.Web/Startup.cs
+++ DNSPod4NETCore2.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace DNSPod4NETCore2.Web
 {
@@ -36,11 +37,11 @@ namespace DNSPod4NETCore2.Web
             services.AddHttpClient<IDnsPodClient, DnsPodClient>();
             services.AddSingleton<DnsPodRecord>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -54,11 +55,11 @@ namespace DNSPod4NETCore2.Web
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
