75d4481d0e1ec498fd5e98724187cb2d9de4b419
算神 <lishewen@msn.com>
2020-10-15 10:40:15 +0800

    有坑 warns CA1416:'Dns.GetHostEntry(string)' is unsupported on 'browser' 挂起

--- DNSPod4NETCore.Blazor/Program.cs
+++ DNSPod4NETCore.Blazor/Program.cs
@@ -1,9 +1,12 @@
+using DNSPod4NETCore2;
+using DNSPod4NETCore2.Web.Models;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +20,28 @@ namespace DNSPod4NETCore.Blazor
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            ConfigureServices(builder.Services);
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            // Example of loading a configuration as configuration isn't available yet at this stage.
+            services.AddSingleton(provider =>
+            {
+                var config = provider.GetService<IConfiguration>();
+                return config.GetSection("DDnsConfiguration").Get<DDnsConfiguration>();
+            });
+            services.AddSingleton(provider =>
+            {
+                var config = provider.GetService<IConfiguration>();
+                return config.GetSection("DnsPodConfiguration").Get<DnsPodConfiguration>();
+            });
+            services.AddHttpClient<IDnsPodClient, DnsPodClient>();
+            services.AddSingleton<DnsPodRecord>();
         }
     }
 }
